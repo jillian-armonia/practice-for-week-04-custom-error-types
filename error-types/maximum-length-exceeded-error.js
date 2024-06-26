0ecad24b96f2f9c8316e5c9926bbeee8bc243021
Jillian Armonia <jillian.marice@gmail.com>
2024-06-26 15:50:10 +0900

    passed the max length exceeded error tests

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,22 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError{
+  constructor(excessLength = null, ...params){
+    super(...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, MaximumLengthExceededError);
+    }
+
+    this.name = 'MaximumLengthExceededError';
+    if (excessLength === null){
+      this.message = `Maximum length exceeded`;
+    } else {
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -2,11 +2,6 @@
 class ValidationError extends Error{
   constructor(...params){
     super(...params);
-
-    if (Error.captureStackTrace){
-      Error.captureStackTrace(this, ValidationError);
-    }
-
     this.name = 'ValidationError';
     this.message = this.message || 'Invalid input';
   }
