4d03a14509c932fab9bdf802b7e7c7d8d7cd6b0c
Jillian Armonia <jillian.marice@gmail.com>
2024-06-26 15:35:44 +0900

    passed validation-error spec

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,18 @@
 // Your code here
+class ValidationError extends Error{
+  constructor(...params){
+    super(...params);
+
+    if (Error.captureStackTrace){
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.name = 'ValidationError';
+    this.message = this.message || 'Invalid input';
+  }
+
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
